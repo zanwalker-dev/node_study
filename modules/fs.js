932691348b9dabc03723dadf32960bb6cb5b3839
zanwalker-dev <andre.rocha2302@gmail.com>
2025-08-06 14:52:17 -0300

    folder created by fs done

--- modules/fs.js
+++ modules/fs.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const path = require("path");
 
+// comentando exemplo abaixo para não ficar dando erro já que o diretorio criado já existe:
 // Criar uma pasta
-fs.mkdir(path.join(__dirname, "/test"), (error) => {
-  if (error) {
-    return console.log("Erro:", error);
-  }
+// fs.mkdir(path.join(__dirname, "/test"), (error) => {
+//   if (error) {
+//     return console.log("Erro:", error);
+//   }
 
-  console.log("Pasta criada com sucesso!");
-});
+//   console.log("Pasta criada com sucesso!");
+// });
