29cf8304cad2a0f5bea5a2560068e29641bea7cb
zanwalker-dev <andre.rocha2302@gmail.com>
2025-08-06 19:02:58 -0300

    feat: readFile node method example

--- modules/fs.js
+++ modules/fs.js
@@ -2,24 +2,46 @@ const fs = require("fs");
 const path = require("path");
 
 // comentando exemplo abaixo para não ficar dando erro já que o diretorio criado já existe:
-// Criar uma pasta
-fs.mkdir(path.join(__dirname, "/test"), (error) => {
-  if (error) {
-    return console.log("Erro:", error);
-  }
+// mkdir Criar uma pasta
+// fs.mkdir(path.join(__dirname, "/test"), (error) => {
+//   if (error) {
+//     return console.log("Erro:", error);
+//   }
 
-  console.log("Pasta criada com sucesso!");
-});
+//   console.log("Pasta criada com sucesso!");
+// });
 
-// criar um file:
-fs.writeFile(
+// // writeFile criar um file:
+// fs.writeFile(
+//   path.join(__dirname, "/test", "test.txt"),
+//   "hello world",
+//   (error) => {
+//     if (error) {
+//       return console.log("Erro:", error);
+//     }
+
+//     console.log("Arquivo e pasta criados com sucesso!");
+//   }
+// );
+
+// appendFile - adicionar a um arquivo
+// fs.appendFile(path.join(__dirname, "/test", "test.txt"), " ,node!", (error) => {
+//   if (error) {
+//     return console.log("Erro:", error);
+//   }
+
+//   console.log("Arquivo modificado com sucesso!");
+// });
+
+// readFile - ler o arquivo:
+fs.readFile(
   path.join(__dirname, "/test", "test.txt"),
-  "hello world",
-  (error) => {
+  "utf8",
+  (error, data) => {
     if (error) {
       return console.log("Erro:", error);
     }
 
-    console.log("Arquivo e pasta criados com sucesso!");
+    console.log(data);
   }
 );
