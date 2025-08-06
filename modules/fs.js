4115a9dcb46f8bb06f10c3680ef11d4eb9a7d8ed
zanwalker-dev <andre.rocha2302@gmail.com>
2025-08-06 14:58:15 -0300

    test file created into test folder with fs

--- modules/fs.js
+++ modules/fs.js
@@ -3,10 +3,23 @@ const path = require("path");
 
 // comentando exemplo abaixo para não ficar dando erro já que o diretorio criado já existe:
 // Criar uma pasta
-// fs.mkdir(path.join(__dirname, "/test"), (error) => {
-//   if (error) {
-//     return console.log("Erro:", error);
-//   }
+fs.mkdir(path.join(__dirname, "/test"), (error) => {
+  if (error) {
+    return console.log("Erro:", error);
+  }
 
-//   console.log("Pasta criada com sucesso!");
-// });
+  console.log("Pasta criada com sucesso!");
+});
+
+// criar um file:
+fs.writeFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "hello world",
+  (error) => {
+    if (error) {
+      return console.log("Erro:", error);
+    }
+
+    console.log("Arquivo e pasta criados com sucesso!");
+  }
+);
