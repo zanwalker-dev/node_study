3f57414b7c429d8ff532d6d361ca9d6dc3f59741
zanwalker-dev <andre.rocha2302@gmail.com>
2025-08-06 14:46:41 -0300

    folder test created by fs mkdir

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const { Person } = require("./person");
 // exemplos de requires:
 // require("./modules/path");
 
+require("./modules/fs");
+
 const person = new Person("André");
 
 // exemplo usando metodo da classe:
