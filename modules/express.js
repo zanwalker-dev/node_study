f1e9b2ce5208376207aa07072c43d81a6ca1705e
zanwalker-dev <andre.rocha2302@gmail.com>
2025-08-07 11:47:54 -0300

    feat: users with express

--- modules/express.js
+++ modules/express.js
@@ -2,11 +2,26 @@ const express = require("express");
 
 const app = express();
 
+const port = 8080;
+
 app.get("/home", (req, res) => {
   res.contentType("application/html");
   res.status(200).send("<h1>Hello world</h1>");
 });
 
-const port = 8080;
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "John Doe",
+      email: "john@doe.com",
+    },
+    {
+      name: "Jane Doe",
+      email: "jane@doe.com",
+    },
+  ];
+
+  res.status(200).json(users);
+});
 
 app.listen(port, () => console.log(`Rodando com express na porta ${port}`));
